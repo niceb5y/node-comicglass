33e07bf9c90c9689724d908cafe34b091b6b18e4
Seungho Kim <niceb5y@gmail.com>
2019-09-21 15:16:53 +0900

    Bump version Add pre-commit lint hook

--- app.js
+++ app.js
@@ -1,59 +1,58 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var fs = require('fs');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var fs = require('fs')
 
-var routes = require('./routes/index');
+var routes = require('./routes/index')
 
-var app = express();
+var app = express()
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-var htpasswd = path.join(__dirname, "user.htpasswd");
+var htpasswd = path.join(__dirname, 'user.htpasswd')
 if (fs.existsSync(htpasswd)) {
-  var auth = require('http-auth');
+  var auth = require('http-auth')
   var basic = auth.basic({
-    realm: "Private Page",
+    realm: 'Private Page',
     file: htpasswd
-  });
-  app.use(auth.connect(basic));
+  })
+  app.use(auth.connect(basic))
 }
 
-app.use('/', routes);
+app.use('/', routes)
 
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err
-    });
-  });
+    })
+  })
 }
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
-});
+  })
+})
 
-
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,68 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
+var express = require('express')
+var router = express.Router()
+var fs = require('fs')
 
 router.get('/*', function(req, res, next) {
-  const path = decodeURIComponent(req.path);
-  const dir = fs.readdirSync(__dirname + '/../public' + path);
-  
+  const path = decodeURIComponent(req.path)
+  const dir = fs.readdirSync(__dirname + '/../public' + path)
+
   function mapFile(filename) {
-    const file = fs.statSync(__dirname + '/../public' + path + filename);
+    const file = fs.statSync(__dirname + '/../public' + path + filename)
     if (file.isDirectory()) {
       return {
         isDirectory: true,
         title: filename,
-        path: encodeURIComponent(path + filename).split("%2F").join("/"),
-        date: (file.mtime.getTime() / 1000)
+        path: encodeURIComponent(path + filename)
+          .split('%2F')
+          .join('/'),
+        date: file.mtime.getTime() / 1000
       }
     } else {
       return {
         isDirectory: false,
         title: filename,
-        path: encodeURIComponent(path + filename).split("%2F").join("/"),
+        path: encodeURIComponent(path + filename)
+          .split('%2F')
+          .join('/'),
         size: file.size,
-        date: (file.mtime.getTime() / 1000)
+        date: file.mtime.getTime() / 1000
       }
     }
   }
-  
+
   function matchExt(file) {
     if (file.title.startsWith('.')) {
-      return false;
+      return false
     }
-    if (file.isDirectory) return true;
-    const supportedExt = [".gif", ".png", ".jpg", ".jpeg", ".tif", ".tiff", ".zip", ".rar", ".cbz", ".cbr", ".bmp", ".pdf", ".cgt", ".webp"];
+    if (file.isDirectory) return true
+    const supportedExt = [
+      '.gif',
+      '.png',
+      '.jpg',
+      '.jpeg',
+      '.tif',
+      '.tiff',
+      '.zip',
+      '.rar',
+      '.cbz',
+      '.cbr',
+      '.bmp',
+      '.pdf',
+      '.cgt',
+      '.webp'
+    ]
     for (var i = 0; i < supportedExt.length; ++i) {
       if (file.title.endsWith(supportedExt[i])) {
-        return true;
+        return true
       }
     }
-    return false;
+    return false
   }
-  
-  res.render('index', {
-    path: "." + path,
-    files: dir.map(mapFile).filter(matchExt)
-  });
-});
 
-module.exports = router;
+  res.render('index', {
+    path: '.' + path,
+    files: dir.map(mapFile).filter(matchExt)
+  })
+})
+
+module.exports = router
